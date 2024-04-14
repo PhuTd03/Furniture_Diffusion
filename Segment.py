25c491d66e1638bbdef65c5e2d33774f285da2a4
PhuTd03 <daiphu220203@gmail.com>
2024-04-14 22:56:30 +0700

    Update Segment.py

--- Segment.py
+++ Segment.py
@@ -3,6 +3,7 @@ sys.path.append("..")
 sys.path.append("./sam")
 from sam.segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 from tool.segmentor import Segmentor
+from tool.detector import Detector
 
 import numpy as np
 
@@ -87,14 +88,14 @@ class Segment():
             masked_frame: numpy array (h, w, c)
         '''
         # get interactive_mask
-        interactive_mask = self.sam.get_interactive_mask(origin_frame, bbox)
+        interactive_mask = self.sam.get_interactive_mask(origin_frame, bbox)[0]
         refined_merged_mask = self.add_mask(interactive_mask)
 
         # draw mask
         masked_frame = draw_mask(origin_frame.copy(), refined_merged_mask)
 
         # draw bbox
-        masked_frame = draw_bbox(bbox, masked_frame)
+        masked_frame = cv2.rectangle(masked_frame, bbox[0], bbox[1], (0, 255, 0))
 
         return refined_merged_mask, masked_frame
 
@@ -143,11 +144,53 @@ class Segment():
 
         refined_merged_mask = self.origin_merged_mask.copy()
         refined_merged_mask[interactive_mask > 0] = self.curr_idx
+
         return refined_merged_mask
         
-    def seg_and_dec():
-        pass
+    def seg_and_dec(self, origin_frame: np.array, prompt_text, box_threshold, text_threshold, box_size_threshold=1, reset_image=False):
+        '''
+        Using Grounding-Dino to detect objec acc Text-Prompt
+        Return:
+            refined_merged_mask: numpy array (h,w)
+            annotated_frame: numpy array (h,w,c)
+        '''
+        # backup id and origin-mergec-mask
+        bc_id = self.curr_idx
+        bc_mask = self.origin_merged_mask
+
+        # get annotated_frame and boxes
+        annotated_frame, boxes = self.detector.run_grounding(origin_frame, prompt_text, box_threshold, text_threshold)
+        for i in range(len(boxes)):
+            bbox = boxes[i]
+            if(bbox[1][0] - bbox[0][0]) * (bbox[1][1] - bbox[0][1]) > annotated_frame.shape[0] * annotated_frame.shape[1] * box_size_threshold:
+                continue
+            interactive_mask = self.sam.segment_with_box(origin_frame, bbox, reset_image)[0]
+            refined_merged_mask = self.add_mask(interactive_mask)
+            self.update_origin_merged_mask(refined_merged_mask)
+            self.curr_idx += 1
+
+        # reset origin mask
+        self.reset_origin_merged_mask(bc_mask, bc_id)
+
+        return refined_merged_mask, annotated_frame
 
 if __name__ == "__main__":
-    
+    from model_args import segment_args, sam_args
 
+    Segment_anything = Segment(segment_args, sam_args)
+
+    # Detect test
+    # test thử ví dụ ở dưới vì khi chạy app.py sẽ không chạy ví dụ mà chỉ lấy hàm để chạy
+    origin_frame = cv2.imread("test/sofa_test/H-5168-12_8c8cc93e-163e-47a6-b8d0-4d3253f0b86b_900x.jpg")
+    origin_frame = cv2.cvtColor(origin_frame, cv2.COLOR_BGR2RGB)
+
+    prompt_text = "sofa"
+    text_threshold = 0.25
+    box_threshold = 0.25
+
+    predicted_mask, annotated_frame = Segment_anything.seg_and_dec(origin_frame, prompt_text, box_threshold, text_threshold)
+    masked_frame = draw_mask(origin_frame, predicted_mask)
+    origin_frame = cv2.cvtColor(origin_frame, cv2.COLOR_RGB2BGR)
+
+    cv2.imwrite('test/sofa_test/masked_frame.jpg', masked_frame)
+    cv2.imwrite('test/sofa_test/x.jpg', annotated_frame)
