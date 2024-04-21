d8054f682e5ed5d1c3cd70ebad93ec60f1a88280
PhuTd03 <daiphu220203@gmail.com>
2024-04-22 04:13:22 +0700

    new commit

--- sam/segment_anything/automatic_mask_generator.py
+++ sam/segment_anything/automatic_mask_generator.py
@@ -274,6 +274,7 @@ class SamAutomaticMaskGenerator:
 
         # Run model on this batch
         transformed_points = self.predictor.transform.apply_coords(points, im_size)
+        transformed_points = transformed_points.astype(np.float32)
         in_points = torch.as_tensor(transformed_points, device=self.predictor.device)
         in_labels = torch.ones(in_points.shape[0], dtype=torch.int, device=in_points.device)
         masks, iou_preds, _ = self.predictor.predict_torch(
